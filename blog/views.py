00aaa9139ec6f5c122d19d9d2c64409e0ff97f01
Evgenii Ikonnikov <evgeniy3129@gmail.com>
2021-04-10 22:48:34 +0200

    Blog paginator is fixed

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Blog
 
 def blog(request):
-    blogs = Blog.objects
+    blogs_list = Blog.objects.order_by('-pub_date')
+    paginator = Paginator(blogs_list, 1)
+
+    page = request.GET.get('page')
+    blogs = paginator.get_page(page)
+
     return render(request, 'blog.html', {'blogs':blogs})
